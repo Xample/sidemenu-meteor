0faa6b9d9fb95e5a22630ed7368ac1761d84e56e
Flavien Volken <flavien.volken@gmail.com>
2014-11-19 17:39:23 +0400

    Fixed dependency

--- client/lib/app.js
+++ client/lib/app.js
@@ -4,6 +4,13 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
+
+    Meteor.startup(function ()
+    {    
+      angular.bootstrap(document, ['starter']);
+    });
+
+
 angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
